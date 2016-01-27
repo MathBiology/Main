b40e9bc477482fedfa0a610f6bc833240f131b88
MathBiology <jmartin2@ualberta.ca>
2016-01-27 18:51:48 +0100

    Update odds.erl

--- ProgrammingErlangBook/odds.erl
+++ ProgrammingErlangBook/odds.erl
@@ -1,6 +1,11 @@
 -module(odds).
--export([odds_and_evens/1]).
-odds_and_evens(L) ->
-Odds = [X || X <- L, (X rem 2) =:= 1],
-Evens = [X || X <- L, (X rem 2) =:= 0],
-{Odds, Evens}.
+-export([odds_and_evens_acc/1]).
+odds_and_evens_acc(L) ->
+odds_and_evens_acc(L, [], []).
+odds_and_evens_acc([H|T], Odds, Evens) ->
+case (H rem 2) of
+1 -> odds_and_evens_acc(T, [H|Odds], Evens);
+0 -> odds_and_evens_acc(T, Odds, [H|Evens])
+end;
+odds_and_evens_acc([], Odds, Evens) ->
+{lists:reverse(Odds), lists:reverse(Evens)}.
