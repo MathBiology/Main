ad19f87e3c33c5b552d6e81a329f6f9b470ca8bb
MathBiology <jmartin2@ualberta.ca>
2016-02-08 09:52:45 +0100

    Generate a random string of fixed length.
    From PHP Cookbook 3rd edition

---
+++ PHPcookbook/randstr.php
@@ -0,0 +1,20 @@
+<?php
+
+function str_rand($length = 32,
+  $characters = 
+  '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+  if (!is_int($length) || $length < 0) {
+    return false;
+  }
+  $characters_length = strlen($characters) - 1;
+  $string = '';
+  for ($i = $length; $i > 0; $i--) {
+    $string .= $characters[mt_rand(0, $characters_length)];
+  }
+  return $string;
+}
+
+$string = str_rand();
+echo $string;
+
+?>
