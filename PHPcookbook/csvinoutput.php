a66868df7ea2f4028ee8230e529bbad073c6624b
MathBiology <jmartin2@ualberta.ca>
2016-02-08 10:31:08 +0100

    Write out CSV from arrays to a file or print back

--- PHPcookbook/csvinoutput.php
+++ PHPcookbook/csvinoutput.php
@@ -14,3 +14,17 @@ die("Can't write CSV line");
 }
 fclose($fh) or die("Can't close $filename");
 ?>
+
+/* Read the data back and print it
+$fp = fopen('./sales.csv','r') or die("can't open file");
+print "<table>\n";
+while($csv_line = fgetcsv($fp)) {
+print '<tr>';
+for ($i = 0, $j = count($csv_line); $i < $j; $i++) {
+print '<td>'.htmlentities($csv_line[$i]).'</td>';
+}
+print "</tr>\n";
+}
+print "</table>\n";
+fclose($fp) or die("can't close file");
+*/
