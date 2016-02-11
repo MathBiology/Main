74636e81ccc6d4a5d6ceccbf832f8b8438aba0a1
MathBiology <jmartin2@ualberta.ca>
2016-02-11 09:33:18 +0100

    Updates username/password info

---
+++ PHPcookbook/EdwinDiazCRUD/update.php
@@ -0,0 +1,37 @@
+<?php include "db.php";?>
+<?php include "functions.php";?>
+
+<?php UpdateTable();?>
+
+<?php include "includes/header.php" ?>
+
+<div class="container">
+    //Creates table for posting updates to username, password, id.
+    <div class="col-sm-6">
+        <h1 class="text-center">Update</h1>
+          <form action="login_update.php" method="post">
+            
+            <div class="form-group">
+              <label for="username">Username</label>
+              <input type="text" name="username" class="form-control">
+              </div>
+            
+             <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" name="password" class="form-control">
+              </div>
+            
+             <div class="form-group">
+               <label for="id">id:</label>
+               <select name="id" id="">
+                  <?php 
+                  showAllData();
+                    ?>
+               </select>
+              </div>
+            
+              <input class="btn btn-primary" type="submit" name="submit" value="UPDATE">
+        </form>
+    </div>
+
+<?php include "includes/footer.php"?>
